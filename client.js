30f99730f4cc9fb4d5e0f61ce19cbf064e814609
sliyoxn <43871064+sliyoxn@users.noreply.github.com>
2021-01-25 19:14:42 +0800

    node

--- client.js
+++ client.js
@@ -1,22 +1,22 @@
 const http = require("http");
-
 const request = http.request(
-  "http://yuanjin.tech:5005/api/movie",
-  {
-    method: "GET"
-  },
-  resp => {
-    console.log("服务器响应的状态码", resp.statusCode);
-    console.log("服务器响应头", resp.headers);
-    let result = "";
-    resp.on("data", chunk => {
-      result += chunk.toString("utf-8");
-    });
-
-    resp.on("end", chunk => {
-      console.log(JSON.parse(result));
-    });
-  }
+    "http://service.picasso.adesk.com/v1/vertical/vertical",
+    {
+        method: "GET"
+    },
+    resp => {
+        console.log("服务器响应的状态码", resp.statusCode);
+        console.log("服务器响应头", resp.headers);
+        let result = "";
+        // 收到服务器响应的数据进行拼接
+        resp.on("data", chunk => {
+            result += chunk.toString("utf-8");
+        });
+        // 服务器响应完了
+        resp.on("end", chunk => {
+            console.log(JSON.parse(result));
+        });
+    }
 );
-
-request.end(); //表示消息体结束
+// request.write(); // 发送请求体，但是get请求没有请求体，这里就不写了
+request.end();  // 表示请求体结束
--- index.js
+++ index.js
@@ -8,56 +8,56 @@ const path = require("path");
 const fs = require("fs");
 
 async function getStat(filename) {
-  try {
-    return await fs.promises.stat(filename);
-  } catch {
-    return null;
-  }
+    try {
+        return await fs.promises.stat(filename);
+    } catch {
+        return null;
+    }
 }
 
 /**
  * 得到要处理的文件内容
  */
 async function getFileContent(url) {
-  const urlObj = URL.parse(url);
-  let filename; //要处理的文件路径
-  filename = path.resolve(__dirname, "public", urlObj.pathname.substr(1));
-  let stat = await getStat(filename);
-  if (!stat) {
-    //文件不存在
-    return null;
-  } else if (stat.isDirectory()) {
-    //文件是一个目录
-    filename = path.resolve(
-      __dirname,
-      "public",
-      urlObj.pathname.substr(1),
-      "index.html"
-    );
-    stat = await getStat(filename);
+    const urlObj = URL.parse(url);
+    let filename; //要处理的文件路径
+    filename = path.resolve(__dirname, "public", urlObj.pathname.substr(1));
+    let stat = await getStat(filename);
     if (!stat) {
-      return null;
+        //文件不存在
+        return null;
+    } else if (stat.isDirectory()) {
+        //文件是一个目录
+        filename = path.resolve(
+            __dirname,
+            "public",
+            urlObj.pathname.substr(1),
+            "index.html"
+        );
+        stat = await getStat(filename);
+        if (!stat) {
+            return null;
+        } else {
+            return await fs.promises.readFile(filename);
+        }
     } else {
-      return await fs.promises.readFile(filename);
+        return await fs.promises.readFile(filename);
     }
-  } else {
-    return await fs.promises.readFile(filename);
-  }
 }
 
 async function handler(req, res) {
-  const info = await getFileContent(req.url);
-  if (info) {
-    res.write(info);
-  } else {
-    res.statusCode = 404;
-    res.write("Resource is not exist");
-  }
-  res.end();
+    const info = await getFileContent(req.url);
+    if (info) {
+        res.write(info);
+    } else {
+        res.statusCode = 404;
+        res.write("Resource is not exist");
+    }
+    res.end();
 }
 
 const server = http.createServer(handler);
 server.on("listening", () => {
-  console.log("server listen 6000");
+    console.log("server listen 6000");
 });
 server.listen(6100);
